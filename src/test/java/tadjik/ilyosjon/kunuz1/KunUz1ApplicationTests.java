f6b077323a7aae9b987b95615e3b4ed72bffba77
Ilyos <ilyoslutfiyev@gmail.com>
2025-03-03 11:11:43 +0500

    Initial commit

---
+++ src/test/java/tadjik/ilyosjon/kunuz1/KunUz1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tadjik.ilyosjon.kunuz1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunUz1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
